9cc668a23247da9a1b7491a2463ca1ccaf2dd9f4
sam <sandervdwindt@gail.com>
2021-06-29 16:01:40 -0400

    ready to deploy

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -61,38 +61,38 @@
 
         <div class="main">
             @if(count($properties) > 0)
-            <div class="properties-grid grid grid-cols-3 gap-2 xl:grid-cols-4 p-4 ">
+            <div class="properties-grid grid grid-cols-2 gap-2 xl:grid-cols-4 p-4 max-w-7xl mx-auto"">
                 @foreach ($properties as $property)
-                <div class="property-card">
-                    <a href="/properties/{{ $property->slug }}">
-                        <h3 class="text-center font-bold text-xl mb-4 truncate">{{ $property->title }}</h3>
-                        {{-- Display Thumbnail --}}
-                        <img src="{{ str_replace ('.jpg', '_thumb.jpg', asset($property->feat_image_path)) }}" alt="{{ ucwords($property->title) }}">
-                    </a>
-                    <div class="property-details py-2">
-                        <div class="main-details flex mb-4 font-bold">
-                            @if($property->status == 'For Sale')
-                            <div class="bg-green-400 text-white capitalize py-1 px-4 mr-4">For Sale</div>
-                            @elseif($property->status == 'Pending')
-                            <div class="bg-yellow-500 text-white capitalize py-1 px-4 mr-4">Pending</div>
-                            @else
-                            <div class="bg-red-400 text-white capitalize py-1 px-4 mr-4">Sold</div>
-                            @endif
-                            <p class="text-center py-1 px-4">{{ ucwords($property->street) }} {{ $property->number
+                <div class=" property-card">
+                <a href="/properties/{{ $property->slug }}">
+                    <h3 class="text-center font-bold text-xl mb-4 truncate">{{ $property->title }}</h3>
+                    {{-- Display Thumbnail --}}
+                    <img src="{{ str_replace ('.jpg', '_thumb.jpg', asset($property->feat_image_path)) }}" alt="{{ ucwords($property->title) }}">
+                </a>
+                <div class="property-details py-2">
+                    <div class="main-details flex mb-4 font-bold">
+                        @if($property->status == 'For Sale')
+                        <div class="bg-green-400 text-white capitalize py-1 px-4 mr-4">For Sale</div>
+                        @elseif($property->status == 'Pending')
+                        <div class="bg-yellow-500 text-white capitalize py-1 px-4 mr-4">Pending</div>
+                        @else
+                        <div class="bg-red-400 text-white capitalize py-1 px-4 mr-4">Sold</div>
+                        @endif
+                        <p class="text-center py-1 px-4">{{ ucwords($property->street) }} {{ $property->number
                                 }}</a></p>
-                        </div>
-                        <p class="text-center font-bold text-gray-500 mb-2">{{ ucwords($property->city) }}, {{
-                            $property->location->name }}</p>
-                        <p class="text-center font-bold"><span class="price"></span>USD &#36; {{
-                            number_format($property->asking_price) }},-</p>
                     </div>
+                    <p class="text-center font-bold text-gray-500 mb-2">{{ ucwords($property->city) }}, {{
+                            $property->location->name }}</p>
+                    <p class="text-center font-bold"><span class="price"></span>USD &#36; {{
+                            number_format($property->asking_price) }},-</p>
                 </div>
-                @endforeach
             </div>
-            @else
-            <h2 class="text-center font-bold text-blue-400 text-xl mt-8">Sorry....Nothing found....</h2>
-            @endif
+            @endforeach
         </div>
+        @else
+        <h2 class="text-center font-bold text-blue-400 text-xl mt-8">Sorry....Nothing found....</h2>
+        @endif
+    </div>
 
     </div>
 </x-guest-layout>
